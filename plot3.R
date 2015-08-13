3ca98b5d22f1250f150d4abdb683228c899f4021
bubblecup <gh@shasha.ca>
2015-08-13 15:14:39 -0400

    optimized
    modified to make more intelligent use of with()

--- plot3.R
+++ plot3.R
@@ -3,13 +3,16 @@ plot3 <- function()
         loadData()
         png(file="plot3.png")
         # Draw main plot
-        with(powerData,plot(Time,Sub_metering_1,type="l",
-                xlab="",ylab="Energy sub metering"))
-        # Draw remaining two sets of data points
-        with(powerData,lines(Time,Sub_metering_2,col="red"))
-        with(powerData,lines(Time,Sub_metering_3,col="blue"))
-        # Draw legend
-        legend("topright",pch=NULL,lwd=1,lty=1,col=c("black","red","blue"),
-               legend=names(powerData[7:9]))
+        with(powerData,
+             {
+                        plot(Time,Sub_metering_1,type="l",
+                                xlab="",ylab="Energy sub metering")
+                        # Draw remaining two sets of data points
+                        lines(Time,Sub_metering_2,col="red")
+                        lines(Time,Sub_metering_3,col="blue")
+                        # Draw legend
+                        legend("topright",pch=NULL,lwd=1,lty=1,col=c("black","red","blue"),
+                                legend=names(powerData[7:9]))
+             })
         dev.off()
 }
